9f986f6154fb4e7e7725d8dd2e10fa67d7dc6bfe
smoladnd <tomica.smola@gmail.com>
2021-11-10 18:48:35 +0100

    Tomislavov (kruzic) treci potez

--- tic-tac-toe/Program.cs
+++ tic-tac-toe/Program.cs
@@ -10,7 +10,7 @@ namespace tic_tac_toe
             Console.WriteLine("Mislav ima 0 pobjeda i 0 poraza!");
 
             Console.WriteLine(" |x| ");
-            Console.WriteLine("0|0| ");
+            Console.WriteLine("0|0|0");
             Console.WriteLine(" |x| ");
         }
     }
